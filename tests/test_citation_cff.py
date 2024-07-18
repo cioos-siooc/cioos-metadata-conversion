543a1ab4644033f8e94049efec7af59974024176
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-07-18 10:48:30 -0400

    drop show_trace

--- tests/test_citation_cff.py
+++ tests/test_citation_cff.py
@@ -55,11 +55,10 @@ def test_hakai_metadata_entry_form_files_cff(file, tmp_path):
     # validate cff
     (tmp_path / "CITATION.cff").write_text(result, encoding="utf-8")
     validation_result = subprocess.run(
-        ["cffconvert", "--validate", "-i", str(tmp_path / "CITATION.cff"), "--show-trace"],
+        ["cffconvert", "--validate", "-i", str(tmp_path / "CITATION.cff")],
         capture_output=True,
     )
     assert validation_result.returncode == 0, validation_result.stderr.decode("utf-8")
-
 
 
 @pytest.mark.parametrize(
