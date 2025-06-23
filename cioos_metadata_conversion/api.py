2345d2f9f381a425e6fec456240b806e23e70e0a
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-06-23 15:36:06 -0400

    Remove commented-out instructions for Sentry data collection in api.py

--- cioos_metadata_conversion/api.py
+++ cioos_metadata_conversion/api.py
@@ -18,8 +18,6 @@ load_dotenv()
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
-    # Add data like request headers and IP for users, if applicable;
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
 )
 
