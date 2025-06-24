72144b8928ce354ea30f1793d97bafad7760d47c
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-06-24 18:44:25 -0400

    ruff formatting

--- cioos_metadata_conversion/datacite.py
+++ cioos_metadata_conversion/datacite.py
@@ -1,5 +1,5 @@
 # Generate a DataCite record from a Cioos record
-# 
+#
 # This follow the DataCite schema v4.6 as described in:
 # https://datacite-metadata-schema.readthedocs.io/en/4.6/properties/overview/
 import json
@@ -36,42 +36,51 @@ CONTRIBUTOR_TYPE_MAPPING_FROM_CIOOS = {
     "processor": "Other",
 }
 
+
 def _get_personal_info(contact) -> dict:
     nameIdentifier = {}
-    if "orcid" in contact['individual']:
+    if "orcid" in contact["individual"]:
         nameIdentifier = {
-            "nameIdentifier": contact['individual']["orcid"],
+            "nameIdentifier": contact["individual"]["orcid"],
             "nameIdentifierScheme": "ORCID",
             "schemeUri": "https://orcid.org",
         }
 
     return {
-        "name": contact['individual']["name"],
+        "name": contact["individual"]["name"],
         "nameType": "Personal",
-        "givenName": contact['individual'].get("givenNames",""),
-        "familyName": contact['individual'].get("lastName",""),
+        "givenName": contact["individual"].get("givenNames", ""),
+        "familyName": contact["individual"].get("lastName", ""),
         "nameIdentifier": nameIdentifier,
     }
+
+
 def _get_organization_info(contact) -> dict:
     return {
-        "name": contact["organization"]['name'],
+        "name": contact["organization"]["name"],
         "nameType": "Organizational",
         "lang": "en",
     }
+
 
 def _get_contact_info(contact) -> dict:
     """
     Get the contact information from the Cioos record.
     """
-    affiliation = {"name": contact["organization"]['name']}
+    affiliation = {"name": contact["organization"]["name"]}
     if "ror" in contact["organization"]:
-        affiliation["affiliationIdentifier"] = contact["organization"]['ror']
+        affiliation["affiliationIdentifier"] = contact["organization"]["ror"]
         affiliation["affiliationIdentifierScheme"] = "ROR"
-        affiliation["schemeUri"] = "https://ror.org/"    
+        affiliation["schemeUri"] = "https://ror.org/"
     return {
-        **(_get_personal_info(contact) if "individual" in contact else _get_organization_info(contact)),
+        **(
+            _get_personal_info(contact)
+            if "individual" in contact
+            else _get_organization_info(contact)
+        ),
         "affiliation": [affiliation],
     }
+
 
 def _get_creators(record) -> list:
     """
@@ -88,6 +97,7 @@ def _get_contributors(record) -> list:
     """
     Get the contributors from the CIOOS record.
     """
+
     def _get_contributor_type(role):
         """
         Get the contributor type from the Cioos record.
@@ -96,7 +106,7 @@ def _get_contributors(record) -> list:
             logger.error(f"Unknown contributor type: {role}")
             return "Other"
         return CONTRIBUTOR_TYPE_MAPPING_FROM_CIOOS[role]
-    
+
     return [
         {
             **_get_contact_info(contact),
@@ -108,43 +118,47 @@ def _get_contributors(record) -> list:
         if role not in {"owner", "publisher", "funder"}
     ]
 
+
 def _get_publisher(record) -> dict:
     for contact in record["contact"]:
         if "publisher" in contact["roles"]:
             publisher = {
-                "name": contact["organization"]['name'],
+                "name": contact["organization"]["name"],
                 "lang": "en",
             }
             if "ror" in contact["organization"]:
-                publisher["publisherIdentifier"] = contact["organization"]['ror']
+                publisher["publisherIdentifier"] = contact["organization"]["ror"]
                 publisher["publisherIdentifierScheme"] = "ROR"
                 publisher["schemeUri"] = "https://ror.org/"
             return publisher
-        
+
     logger.warning("No publisher found in the record.")
+
 
 def _get_funding_references(record) -> dict:
     """
     Get the funding references from the Cioos record.
     """
+
     def _get_funder_ror(contact) -> dict:
-        if not contact["organization"].get('ror'):
+        if not contact["organization"].get("ror"):
             return {}
-        return{
-            "funderIdentifier": contact["organization"]['ror'],
+        return {
+            "funderIdentifier": contact["organization"]["ror"],
             "funderIdentifierType": "ROR",
         }
 
     return {
         "fundingReferences": [
             {
-                "funderName": contact["organization"]['name'],
+                "funderName": contact["organization"]["name"],
                 **_get_funder_ror(contact),
             }
             for contact in record["contact"]
             if "funder" in contact["roles"]
         ]
     }
+
 
 def _get_subject_scheme(group) -> dict:
     """
@@ -164,19 +178,21 @@ def _get_subject_scheme(group) -> dict:
         return {}
     else:
         logger.error(f"Unknown subject group: {group}")
-        return {
-            
-        }
+        return {}
+
 
 DATES_MAPPING = {
     "creation": "Created",
     "publication": "Issued",
     "revision": "Updated",
 }
+
+
 def _get_dates(record) -> list:
     """
     Get the dates from the Cioos record.
     """
+
     def _get_date(name, date):
         """
         Get the date from the Cioos record.
@@ -194,53 +210,60 @@ def _get_dates(record) -> list:
         }
 
     return (
-        [_get_date(name, date)
-        for name, date in record['identification']["dates"].items()] +
-        [_get_date(name,date)
-         for name, date in record['metadata']["dates"].items()] +
-        [{
-            "date": f"{record['identification'].get('temporal_begin','*')}/{record['identification'].get('temporal_end', '*')}",
-            "dateType": "Collected",
-        }]
+        [
+            _get_date(name, date)
+            for name, date in record["identification"]["dates"].items()
+        ]
+        + [_get_date(name, date) for name, date in record["metadata"]["dates"].items()]
+        + [
+            {
+                "date": f"{record['identification'].get('temporal_begin','*')}/{record['identification'].get('temporal_end', '*')}",
+                "dateType": "Collected",
+            }
+        ]
     )
+
 
 def _get_alternate_identifiers(record) -> dict:
     """
     Get the alternate identifiers from the Cioos record.
     """
-    return {"alternateIdentifiers":[
-    ]}
+    return {"alternateIdentifiers": []}
+
 
 def _get_related_identifiers(record) -> dict:
     """
     Get the related identifiers from the Cioos record.
     """
-    return {"relatedIdentifiers":[
-    ]}
+    return {"relatedIdentifiers": []}
+
 
 def _get_related_items(record) -> dict:
     """
     Get the related items from the Cioos record.
     """
-    return {"relatedItems":[
-    ]}
+    return {"relatedItems": []}
 
 
 def generate_record(record) -> dict:
     """
     Generate a DataCite record from a Cioos record.
     """
+
     def _add_optional(field, value):
         """
         Add an optional field to the record.
         """
         if not value:
             logger.debug(f"Optional field {field} is empty")
-            return 
+            return
         optional_fields[field] = value
-        
+
     optional_fields = {}
-    _add_optional("doi", record["identification"].get("identifier","").replace("https://doi.org/",""))
+    _add_optional(
+        "doi",
+        record["identification"].get("identifier", "").replace("https://doi.org/", ""),
+    )
 
     return {
         "titles": [
@@ -248,7 +271,7 @@ def generate_record(record) -> dict:
                 "title": title,
                 "lang": lang,
             }
-            for lang, title in record['identification']["title"].items()
+            for lang, title in record["identification"]["title"].items()
             if lang != "translations"
         ],
         **optional_fields,
@@ -256,24 +279,26 @@ def generate_record(record) -> dict:
         "publisher": _get_publisher(record),
         "contributors": _get_contributors(record),
         # parse iso date and return year from record['identification']["dates"]["created"]
-        "publicationYear": str(datetime.strptime(
-            record['metadata']["dates"]["publication"], "%Y-%m-%d"
-        ).year),
+        "publicationYear": str(
+            datetime.strptime(
+                record["metadata"]["dates"]["publication"], "%Y-%m-%d"
+            ).year
+        ),
         "subjects": [
             {
                 "subject": keyword,
                 "lang": lang,
                 **_get_subject_scheme(group),
             }
-            for group, group_keywords in record['identification']["keywords"].items()
+            for group, group_keywords in record["identification"]["keywords"].items()
             for lang, keywords in group_keywords.items()
             for keyword in keywords
         ],
-        "dates":_get_dates(record),
-        "language": record['metadata']["language"],
+        "dates": _get_dates(record),
+        "language": record["metadata"]["language"],
         "types": {
-            "resourceTypeGeneral": "Dataset", # TODO revise with latest version of cioos
-            "resourceType": "CIOOS Dataset Record", # TODO revise with latest version of cioos
+            "resourceTypeGeneral": "Dataset",  # TODO revise with latest version of cioos
+            "resourceType": "CIOOS Dataset Record",  # TODO revise with latest version of cioos
         },
         **_get_alternate_identifiers(record),
         **_get_related_identifiers(record),
@@ -282,12 +307,16 @@ def generate_record(record) -> dict:
         "version": record["identification"]["edition"],
         "rightsList": [
             {
-                "rights": record["metadata"]['use_constraints']['licence']['title']['en'],
-                "rightsUri": record["metadata"]['use_constraints']['licence']['url'],
-                "schemeUri": "https://spdx.org/licenses/", # TODO confirm
-                "rightsIdentifier": record["metadata"]['use_constraints']['licence']['code'],
-                "rightsIdentifierScheme": "SPDX", # TODO confirm 
-                "lang": "en", 
+                "rights": record["metadata"]["use_constraints"]["licence"]["title"][
+                    "en"
+                ],
+                "rightsUri": record["metadata"]["use_constraints"]["licence"]["url"],
+                "schemeUri": "https://spdx.org/licenses/",  # TODO confirm
+                "rightsIdentifier": record["metadata"]["use_constraints"]["licence"][
+                    "code"
+                ],
+                "rightsIdentifierScheme": "SPDX",  # TODO confirm
+                "lang": "en",
             }
         ],
         "descriptions": [
@@ -298,23 +327,30 @@ def generate_record(record) -> dict:
             }
             for lang, abstract in record["identification"]["abstract"].items()
             if lang != "translations"
-        ] + [
+        ]
+        + [
             {
                 "description": "limitations: " + description,
                 "lang": lang,
                 "descriptionType": "Other",
             }
-            for lang, description in record["metadata"]["use_constraints"]['limitations'].items()
+            for lang, description in record["metadata"]["use_constraints"][
+                "limitations"
+            ].items()
             if lang != "translations"
         ],
         "geoLocations": [
-            {"geoLocationPolygon":[
-                {"polygonPoint": {
-                    "pointLatitude": float(loc.split(',')[1]),
-                    "pointLongitude": float(loc.split(',')[0]),
-                }}
-                for loc in record["spatial"]["polygon"].split(" ")
-            ]}
+            {
+                "geoLocationPolygon": [
+                    {
+                        "polygonPoint": {
+                            "pointLatitude": float(loc.split(",")[1]),
+                            "pointLongitude": float(loc.split(",")[0]),
+                        }
+                    }
+                    for loc in record["spatial"]["polygon"].split(" ")
+                ]
+            }
         ],
         **_get_funding_references(record),
         **_get_related_items(record),
@@ -340,7 +376,7 @@ def to_xml(record, output=None) -> str:
     """
     datacite_record = generate_record(record)
     xml = schema45.tostring(datacite_record)
-    
+
     if output:
         logger.debug(f"Output file: {output}")
         with open(output, "w") as f:
--- tests/test_datacite.py
+++ tests/test_datacite.py
@@ -1,13 +1,14 @@
 from cioos_metadata_conversion import datacite
 from datacite import schema45
 
+
 def test_dataset_cite(record):
     """
     Test the dataset citation generation.
     """
     datacite_record = datacite.generate_record(record)
     assert datacite_record
-    
+
     # validate schema
     schema45.validator.validate(datacite_record)
 
@@ -18,10 +19,11 @@ def test_json_output(record, tmp_path):
     """
     test_file = tmp_path / "test.json"
     json_output = datacite.to_json(record, test_file)
-    
+
     assert json_output
     assert isinstance(json_output, dict)  # Ensure it's a string
     assert test_file.exists()  # Ensure the path exists
+
 
 def test_xml_output(record, tmp_path):
     """
@@ -29,7 +31,7 @@ def test_xml_output(record, tmp_path):
     """
     test_file = tmp_path / "test.xml"
     xml_output = datacite.to_xml(record, test_file)
-    
+
     assert xml_output
     assert isinstance(xml_output, str)  # Ensure it's a string
     assert test_file.exists()  # Ensure the path exists
