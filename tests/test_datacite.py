316ce580e40ad71aaf4e44fe303727869834b075
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-07-04 14:01:20 -0400

    add datacite tests with firebase

--- tests/test_datacite.py
+++ tests/test_datacite.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+import json
+
 from datacite import schema45
+import pytest
 
 from cioos_metadata_conversion import datacite
+from firebase_to_xml.record_json_to_yaml import record_json_to_yaml
 
 
 def test_dataset_cite(record):
@@ -36,3 +41,37 @@ def test_xml_output(record, tmp_path):
     assert xml_output
     assert isinstance(xml_output, str)  # Ensure it's a string
     assert test_file.exists()  # Ensure the path exists
+
+@pytest.mark.parametrize("firebase_record",(Path(__file__).parent / "records" / "firebase" ).glob("*.json"))
+def test_firebase_record_to_xml(firebase_record):
+    """
+    Test the conversion of a Firebase record to XML.
+    """
+    with open(firebase_record, 'r') as f:
+        record = json.load(f)
+    record = record_json_to_yaml(record)
+
+    # Convert the record to XML
+    test_file = firebase_record.with_suffix('.xml')
+    xml_output = datacite.to_xml(record, test_file)
+
+    assert xml_output
+    assert isinstance(xml_output, str)  # Ensure it's a string
+    assert test_file.exists()  # Ensure the path exists
+
+
+@pytest.mark.parametrize("firebase_record",(Path(__file__).parent / "records" / "firebase" ).glob("*.json"))
+def test_firebase_record_schema(firebase_record):
+    """
+    Test the conversion of a Firebase record to XML.
+    """
+    with open(firebase_record, 'r') as f:
+        record = json.load(f)
+    record = record_json_to_yaml(record)
+
+    # Convert the record to XML
+    test_file = firebase_record.with_suffix('.xml')
+    datacite_record = datacite.generate_record(record)
+
+    assert datacite_record
+    schema45.validator.validate(datacite_record)
